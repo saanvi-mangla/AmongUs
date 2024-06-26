f856258f875eaa578f0ec0fd4b13954d5f765c5c
unjoinable <135444615+unjoinable@users.noreply.github.com>
2024-06-26 16:30:47 +0530

    Update PlayerHeader.java

--- src/main/java/io/github/unjoinable/amongus/util/PlayerHeader.java
+++ src/main/java/io/github/unjoinable/amongus/util/PlayerHeader.java
@@ -7,6 +7,8 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.network.packet.server.play.TeamsPacket;
 import net.minestom.server.scoreboard.TeamBuilder;
 
+import java.util.UUID;
+
 import static io.github.unjoinable.amongus.util.MiniMessageTemplate.MM;
 
 public class PlayerHeader {
@@ -16,16 +18,15 @@ public class PlayerHeader {
      * @param crewmate The type of crewmate player is / will be
      */
     public static void setSussyHeader(Player player, Crewmate crewmate) {
-        PlayerHeader.setPlayerHeader(player, crewmate.name(), MM."<bold>\{crewmate.name().replaceAll("_", " ")}".color(crewmate.getColor()));
+        PlayerHeader.setPlayerHeader(player, MM."<bold>\{crewmate.name().replaceAll("_", " ")}".color(crewmate.getColor()));
     }
 
     /**
      * @param player The player who we are adding to team
-     * @param header The name of team
      * @param display The prefix of team
      */
-    public static void setPlayerHeader(Player player, String header, Component display) {
-        TeamBuilder builder = MinecraftServer.getTeamManager().createBuilder(header);
+    public static void setPlayerHeader(Player player, Component display) {
+        TeamBuilder builder = MinecraftServer.getTeamManager().createBuilder(UUID.randomUUID().toString());
         builder.nameTagVisibility(TeamsPacket.NameTagVisibility.ALWAYS);
         builder.prefix(display);
         builder.collisionRule(TeamsPacket.CollisionRule.ALWAYS);
